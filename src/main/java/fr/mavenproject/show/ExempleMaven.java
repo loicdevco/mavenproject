5f02ba3f24dc9369e48c4ece449cbf52b246122c
lolo <63410056+loicdevco@users.noreply.github.com>
2022-09-08 12:00:04 +0200

    initial commit

---
+++ src/main/java/fr/mavenproject/show/ExempleMaven.java
@@ -0,0 +1,14 @@
+package fr.mavenproject.show;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+public class ExempleMaven {
+
+	public static void main(String[] args) {
+		ImagePlus imp = IJ.openImage();
+		imp.show();
+
+	}
+
+}
